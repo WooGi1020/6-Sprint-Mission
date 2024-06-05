dd0562289c61927aa7f7b31793f9d1b692e8ef6f
WooGi1020 <jungwk1020@gmail.com>
2024-06-05 21:18:49 +0900

    ✨ feat : 상세 게시글 및 댓글 받아오는 API 로직 추가

--- lib/apis/api.ts
+++ lib/apis/api.ts
@@ -1,5 +1,6 @@
 import instance from "@/lib/axios";
 import { AxiosResponse } from "axios";
+import { ArticleValue } from "@/pages/boards/addBoards";
 
 export type ArticleResponse = {
   id: number;
@@ -22,6 +23,7 @@ export type ArticlesResponse = {
   totalCount: number;
 };
 
+// 게시글 목록 받아오는 API
 export async function getArticles({
   orderBy = "recent",
   keyword = "",
@@ -45,6 +47,18 @@ export async function getArticles({
   }
 }
 
+// 단일 게시글 받아오는 API
+export async function getArticle(id: string | string[] | undefined) {
+  try {
+    const response: AxiosResponse<ArticleResponse> = await instance.get(`/${id}`);
+    return response.data;
+  } catch (e) {
+    console.log(`error : ${e}`);
+    throw new Error();
+  }
+}
+
+// 베스트 게시글 목록 받아오는 API
 export async function getBestArticles(pageSize: number) {
   const params = new URLSearchParams({
     page: "1",
@@ -53,6 +67,59 @@ export async function getBestArticles(pageSize: number) {
   });
   try {
     const response: AxiosResponse<ArticlesResponse> = await instance.get(`?${params.toString()}`);
+    return response.data;
+  } catch (e) {
+    console.error(`error : ${e}`);
+    throw new Error();
+  }
+}
+
+export type CommentsResponse = {
+  list: CommentResponse[];
+  nextCursor: number;
+};
+
+export type CommentResponse = {
+  id: number;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+  writer: commentWriterResponse;
+};
+
+export type commentWriterResponse = {
+  image: string;
+} & WriterResponse;
+// 게시글 댓글 목록 받아오는 API
+export async function getArticleComments(id: string | string[] | undefined, limit: 10) {
+  try {
+    const response: AxiosResponse<CommentsResponse> = await instance.get(`/${id}/comments?limit=${limit}`);
+    return response.data;
+  } catch (e) {
+    console.error(`error : ${e}`);
+    throw new Error();
+  }
+}
+
+type FormDataResponse = {
+  updatedAt: string;
+  createdAt: string;
+  likeCount: number;
+  writer: {
+    nickname: string;
+    id: number;
+  };
+  image: string;
+  content: string;
+  title: string;
+
+  id: number;
+};
+
+// 게시글 등록용 POST API
+export async function postArticle(formData: ArticleValue) {
+  try {
+    const response: AxiosResponse<FormDataResponse> = await instance.post(``, formData);
     return response.data;
   } catch (e) {
     console.error(`error : ${e}`);
