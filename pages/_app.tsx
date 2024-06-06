356984385a07875c5a42a4df0e6b52b930a44675
WooGi1020 <jungwk1020@gmail.com>
2024-06-06 22:08:33 +0900

    ✨ feat : 레이아웃 제외 로직 및 스크롤바 고정 로직 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,45 @@ import "@/styles/globals.css";
 import Header from "./Header";
 import Footer from "./Footer";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { Router } from "next/router";
 
-export default function App({ Component, pageProps }: AppProps) {
+type CustomAppProps = AppProps & {
+  Component: AppProps["Component"] & { noLayout?: boolean };
+};
+
+export default function App({ Component, pageProps }: CustomAppProps) {
+  const useLayout = !Component.noLayout;
+
+  useEffect(() => {
+    if ("scrollRestoration" in history) {
+      history.scrollRestoration = "manual";
+      const saveScrollPosition = () => {
+        sessionStorage.setItem("scrollPosition", JSON.stringify({ x: window.scrollX, y: window.scrollY }));
+      };
+      const restoreScrollPosition = () => {
+        const scrollPosition = JSON.parse(sessionStorage.getItem("scrollPosition") || "null");
+        if (scrollPosition) {
+          window.scrollTo(scrollPosition.x, scrollPosition.y);
+        }
+      };
+
+      Router.events.on("routeChangeStart", saveScrollPosition);
+      Router.events.on("routeChangeComplete", restoreScrollPosition);
+      Router.events.on("beforeHistoryChange", saveScrollPosition);
+
+      return () => {
+        Router.events.off("routeChangeStart", saveScrollPosition);
+        Router.events.off("routeChangeComplete", restoreScrollPosition);
+        Router.events.off("beforeHistoryChange", saveScrollPosition);
+      };
+    }
+  }, []);
+
+  if (!useLayout) {
+    return <Component {...pageProps} />;
+  }
+
   return (
     <>
       <Header />
