ac70383931fd78495a98b6baec656e4706ae8914
WooGi1020 <jungwk1020@gmail.com>
2024-06-11 09:59:09 +0900

    ✨ feat : Head 태그 추가 및 타이틀 설정

--- lib/axios.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-const instance = axios.create({
-  baseURL: API_URL,
-});
-
-export default instance;
--- pages/SignUp.tsx
+++ pages/SignUp.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { signUp } from "@/lib/apis/sign.api";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 const SignUp = () => {
   const [isSignUpBtnDisabled, setIsSignUpBtnDisabled] = useState(true);
@@ -108,143 +109,153 @@ const SignUp = () => {
   };
 
   return (
-    <div className={styles["sign-up-container"]}>
-      <div className={styles["sign-up-container__logo"]}>
-        <Link href="/">
-          <Image
-            src="/images/Sign/panda-market-logo.png"
-            alt="판다마켓 로고 겸 메인 페이지 이동 버튼"
-            fill
-            style={{ objectFit: "cover" }}
-          />
-        </Link>
-      </div>
-
-      <form action="#" method="post" className={styles["sign-up-from"]} onSubmit={handleSubmit}>
-        <div className={styles["con"]}>
-          <label htmlFor="email">이메일</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="이메일을 입력해주세요"
-            onChange={checkInputValidity}
-            className={styles[isValidEmail ? "sign-email-input" : "sign-email-input-wrong"]}
-            autoFocus
-          />
-          {isValidEmail || (
-            <span className={styles["wrong-email"]}>
-              {signUpInfo.email === "" ? "이메일을 입력해주세요" : "잘못된 이메일 형식입니다"}
-            </span>
-          )}
+    <>
+      <Head>
+        <title>회원가입</title>
+      </Head>
+      <div className={styles["sign-up-container"]}>
+        <div className={styles["sign-up-container__logo"]}>
+          <Link href="/">
+            <Image
+              src="/images/Sign/panda-market-logo.png"
+              alt="판다마켓 로고 겸 메인 페이지 이동 버튼"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </Link>
         </div>
 
-        <div className={styles["con"]}>
-          <label htmlFor="nickname">닉네임</label>
-          <input
-            type="text"
-            id="nickname"
-            name="nickname"
-            placeholder="닉네임을 입력해주세요"
-            className={styles[isValidNickname ? "sign-nick-input" : "sign-nick-input-wrong"]}
-            onChange={checkInputValidity}
-          />
-          {isValidNickname || (
-            <span className={styles["wrong-nick"]}>{signUpInfo.nickname === "" && "닉네임을 입력해주세요"}</span>
-          )}
-        </div>
-
-        <div className={styles.con}>
-          <label htmlFor="pw">비밀번호</label>
-          <input
-            type={isPwShow.password ? "text" : "password"}
-            id="pw"
-            name="pw"
-            placeholder="비밀번호를 입력해주세요"
-            className={styles[isValidPw ? "sign-pw-input" : "sign-pw-input-wrong"]}
-            onChange={checkInputValidity}
-          />
-          {isPwShow.password ? (
-            <Image
-              src="/images/Sign/show-icon.svg"
-              alt="비밀번호 보이기 버튼"
-              width={24}
-              height={24}
-              className={styles["show-icon"]}
-              onClick={handlePwShow}
-            ></Image>
-          ) : (
-            <Image
-              src="/images/Sign/none-show-icon.svg"
-              alt="비밀번호 숨기기 버튼"
-              width={24}
-              height={24}
-              className={styles["none-show-icon"]}
-              onClick={handlePwShow}
-            ></Image>
-          )}
-          {isValidPw || (
-            <span className={styles["wrong-pw"]}>
-              {signUpInfo.password === "" ? "비밀번호를 입력해주세요" : "비밀번호를 8자 이상 입력해주세요"}
-            </span>
-          )}
-        </div>
-
-        <div className={styles.con}>
-          <label htmlFor="re-pw">비밀번호 확인</label>
-          <input
-            type={isPwShow.passwordConfirmation ? "text" : "password"}
-            id="re-pw"
-            name="re-pw"
-            placeholder="비밀번호를 다시 한번 입력해주세요"
-            className={styles[isPwMatch ? "sign-repw-input" : "sign-repw-input-wrong"]}
-            onChange={checkInputValidity}
-          />
-          {isPwShow.passwordConfirmation ? (
-            <Image
-              src="/images/Sign/show-icon.svg"
-              alt="비밀번호 보이기 버튼"
-              width={24}
-              id="confirm"
-              height={24}
-              className={styles["show-icon"]}
-              onClick={handlePwShow}
-            ></Image>
-          ) : (
-            <Image
-              src="/images/Sign/none-show-icon.svg"
-              alt="비밀번호 숨기기 버튼"
-              width={24}
-              height={24}
-              id="confirm"
-              className={styles["none-show-icon"]}
-              onClick={handlePwShow}
-            ></Image>
-          )}
-          {isPwMatch || <span className={styles["wrong-repw"]}>비밀번호가 일치하지 않습니다</span>}
-        </div>
-
-        <button type="submit" disabled={isSignUpBtnDisabled} className={styles["signup-btn"]}>
-          회원가입
-        </button>
-
-        <div className={styles["social-login"]}>
-          <p>간편 로그인하기</p>
-          <div className={styles["social-login-logo"]}>
-            <Link href="https://accounts.google.com/signin/v2/usernamerecovery?continue=https%3A%2F%2Faccounts.google.com%2FManageAccount&hl=ko&theme=glif&ddm=0&flowName=GlifWebSignIn&flowEntry=ServiceLogin">
-              <Image src="/images/Sign/google-logo.png" alt="구글 로그인 이동하기 버튼" width={42} height={42}></Image>
-            </Link>
-            <Link href="https://accounts.kakao.com/login/?continue=https%3A%2F%2Fkauth.kakao.com%2Foauth%2Fauthorize%3Fproxy%3DeasyXDM_Kakao_9iyy5od3l29_provider%26ka%3Dsdk%252F1.43.2%2520os%252Fjavascript%2520sdk_type%252Fjavascript%2520lang%252Fko-KR%2520device%252FWin32%2520origin%252Fhttps%25253A%25252F%25252Fwww.epis.or.kr%26origin%3Dhttps%253A%252F%252Fwww.epis.or.kr%26response_type%3Dcode%26redirect_uri%3Dkakaojs%26state%3Dlgo6gi4qr2byw694fiizh%26through_account%3Dtrue%26client_id%3D950f3ae1f2f550e5ba7ff7d9fffc7781&talk_login=hidden#login">
-              <Image src="/images/Sign/kakao-logo.png" alt="구글 로그인 이동하기 버튼" width={42} height={42}></Image>
-            </Link>
+        <form action="#" method="post" className={styles["sign-up-from"]} onSubmit={handleSubmit}>
+          <div className={styles["con"]}>
+            <label htmlFor="email">이메일</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="이메일을 입력해주세요"
+              onChange={checkInputValidity}
+              className={styles[isValidEmail ? "sign-email-input" : "sign-email-input-wrong"]}
+              autoFocus
+            />
+            {isValidEmail || (
+              <span className={styles["wrong-email"]}>
+                {signUpInfo.email === "" ? "이메일을 입력해주세요" : "잘못된 이메일 형식입니다"}
+              </span>
+            )}
           </div>
-        </div>
-        <div className={styles["go-signin-container"]}>
-          <p>이미 회원이신가요?</p>
-          <Link href="/SignIn">로그인</Link>
-        </div>
-      </form>
-    </div>
+
+          <div className={styles["con"]}>
+            <label htmlFor="nickname">닉네임</label>
+            <input
+              type="text"
+              id="nickname"
+              name="nickname"
+              placeholder="닉네임을 입력해주세요"
+              className={styles[isValidNickname ? "sign-nick-input" : "sign-nick-input-wrong"]}
+              onChange={checkInputValidity}
+            />
+            {isValidNickname || (
+              <span className={styles["wrong-nick"]}>{signUpInfo.nickname === "" && "닉네임을 입력해주세요"}</span>
+            )}
+          </div>
+
+          <div className={styles.con}>
+            <label htmlFor="pw">비밀번호</label>
+            <input
+              type={isPwShow.password ? "text" : "password"}
+              id="pw"
+              name="pw"
+              placeholder="비밀번호를 입력해주세요"
+              className={styles[isValidPw ? "sign-pw-input" : "sign-pw-input-wrong"]}
+              onChange={checkInputValidity}
+            />
+            {isPwShow.password ? (
+              <Image
+                src="/images/Sign/show-icon.svg"
+                alt="비밀번호 보이기 버튼"
+                width={24}
+                height={24}
+                className={styles["show-icon"]}
+                onClick={handlePwShow}
+              ></Image>
+            ) : (
+              <Image
+                src="/images/Sign/none-show-icon.svg"
+                alt="비밀번호 숨기기 버튼"
+                width={24}
+                height={24}
+                className={styles["none-show-icon"]}
+                onClick={handlePwShow}
+              ></Image>
+            )}
+            {isValidPw || (
+              <span className={styles["wrong-pw"]}>
+                {signUpInfo.password === "" ? "비밀번호를 입력해주세요" : "비밀번호를 8자 이상 입력해주세요"}
+              </span>
+            )}
+          </div>
+
+          <div className={styles.con}>
+            <label htmlFor="re-pw">비밀번호 확인</label>
+            <input
+              type={isPwShow.passwordConfirmation ? "text" : "password"}
+              id="re-pw"
+              name="re-pw"
+              placeholder="비밀번호를 다시 한번 입력해주세요"
+              className={styles[isPwMatch ? "sign-repw-input" : "sign-repw-input-wrong"]}
+              onChange={checkInputValidity}
+            />
+            {isPwShow.passwordConfirmation ? (
+              <Image
+                src="/images/Sign/show-icon.svg"
+                alt="비밀번호 보이기 버튼"
+                width={24}
+                id="confirm"
+                height={24}
+                className={styles["show-icon"]}
+                onClick={handlePwShow}
+              ></Image>
+            ) : (
+              <Image
+                src="/images/Sign/none-show-icon.svg"
+                alt="비밀번호 숨기기 버튼"
+                width={24}
+                height={24}
+                id="confirm"
+                className={styles["none-show-icon"]}
+                onClick={handlePwShow}
+              ></Image>
+            )}
+            {isPwMatch || <span className={styles["wrong-repw"]}>비밀번호가 일치하지 않습니다</span>}
+          </div>
+
+          <button type="submit" disabled={isSignUpBtnDisabled} className={styles["signup-btn"]}>
+            회원가입
+          </button>
+
+          <div className={styles["social-login"]}>
+            <p>간편 로그인하기</p>
+            <div className={styles["social-login-logo"]}>
+              <Link href="https://accounts.google.com/signin/v2/usernamerecovery?continue=https%3A%2F%2Faccounts.google.com%2FManageAccount&hl=ko&theme=glif&ddm=0&flowName=GlifWebSignIn&flowEntry=ServiceLogin">
+                <Image
+                  src="/images/Sign/google-logo.png"
+                  alt="구글 로그인 이동하기 버튼"
+                  width={42}
+                  height={42}
+                ></Image>
+              </Link>
+              <Link href="https://accounts.kakao.com/login/?continue=https%3A%2F%2Fkauth.kakao.com%2Foauth%2Fauthorize%3Fproxy%3DeasyXDM_Kakao_9iyy5od3l29_provider%26ka%3Dsdk%252F1.43.2%2520os%252Fjavascript%2520sdk_type%252Fjavascript%2520lang%252Fko-KR%2520device%252FWin32%2520origin%252Fhttps%25253A%25252F%25252Fwww.epis.or.kr%26origin%3Dhttps%253A%252F%252Fwww.epis.or.kr%26response_type%3Dcode%26redirect_uri%3Dkakaojs%26state%3Dlgo6gi4qr2byw694fiizh%26through_account%3Dtrue%26client_id%3D950f3ae1f2f550e5ba7ff7d9fffc7781&talk_login=hidden#login">
+                <Image src="/images/Sign/kakao-logo.png" alt="구글 로그인 이동하기 버튼" width={42} height={42}></Image>
+              </Link>
+            </div>
+          </div>
+          <div className={styles["go-signin-container"]}>
+            <p>이미 회원이신가요?</p>
+            <Link href="/SignIn">로그인</Link>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
